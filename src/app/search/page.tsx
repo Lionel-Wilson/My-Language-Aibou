1429cdd077c2d41c3053e0e60915247af3cdbe5a
Lionel Wilson <lionel_wilson@outlook.com>
2024-07-14 04:35:42 +0100

    update css at 1280px width

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -100,7 +100,7 @@ export default function Page() {
           </div>
 
           {/* Page content here */}
-          <div className="min-[410px]:flex min-[410px]:justify-center ml-6 min-[360px]:ml-11 min-[375px]:ml-14 min-[390px]:ml-16 min-[410px]:ml-0 sm:ml-8 md:ml-10 my-20 md:my-40">
+          <div className="min-[410px]:flex min-[410px]:justify-center ml-6 min-[360px]:ml-11 min-[375px]:ml-14 min-[390px]:ml-16 min-[410px]:ml-0 sm:ml-8 md:ml-10 lg:ml-36 xl:ml-60 my-20 md:my-40">
             <div role="tablist" className="tabs tabs-lifted w-2/3 md:tabs-lg">
               <input
                 type="radio"
