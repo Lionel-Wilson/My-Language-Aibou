cf88de291b350f0726e2afe6959038273703d64f
Lionel Wilson <lionel_wilson@outlook.com>
2024-07-17 14:55:08 +0100

    Only convert dashes at the beginning of sentences to bullet points

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -50,7 +50,7 @@ export default function Page() {
   function formatExpression(expression: string): string {
     return expression
       .replace(/###\s*(.*?):/g, "<h1><strong>$1:</strong></h1>") // Convert ### followed by text and a colon into H1 title
-      .replace(/ - /g, " • ") // Convert dashes to bullet points
+      .replace(/^\s*-\s*/gm, "• ") // Convert dashes at the beginning of lines to bullet points
       .replace(/\n/g, "<br>") // Line breaks
       .replace(/"/g, "&quot;") // Escape double quotes for HTML
       .replace(/'/g, "&#39;") // Escape single quotes for HTML
