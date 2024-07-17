70719896982c2d8124600a108144addf104d9e71
Lionel Wilson <lionel_wilson@outlook.com>
2024-07-17 14:48:35 +0100

    Turn ###title into h1

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -49,6 +49,7 @@ export default function Page() {
 
   function formatExpression(expression: string): string {
     return expression
+      .replace(/###\s*(.*?):/g, "<h1><strong>$1:</strong></h1>") // Convert ### followed by text and a colon into H1 title
       .replace(/ - /g, " • ") // Convert dashes to bullet points
       .replace(/\n/g, "<br>") // Line breaks
       .replace(/"/g, "&quot;") // Escape double quotes for HTML
