7eccc044c71cc9f2ce3ef6843c6ad7f9d771e7a0
Lionel Wilson <lionel_wilson@outlook.com>
2024-07-22 02:38:21 +0100

    remove code comments

--- src/app/search/actions.ts
+++ src/app/search/actions.ts
@@ -26,43 +26,11 @@ export async function analyseExpression(prevState: any, formData: FormData) {
   }
 
   var answer = await response.json();
-  //console.log("Answer:");
-  //console.log(answer);
+
   return {
     expressionAnswer: answer,
     error: null,
   };
-
-  /*
-  try {
-    const response = await fetch(`${process.env.API_URL}/search/sentence`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(rawFormData),
-      cache: "no-store",
-    });
-
-    if (!response.ok) {
-      throw new Error("Failed to fetch data");
-    }
-
-    var answer = await response.json();
-    //console.log("Answer:");
-    //console.log(answer);
-    return {
-      expressionAnswer: answer,
-      error: null,
-    };
-  } catch (error) {
-    console.error("Error:", error);
-    return {
-      expressionAnswer: null,
-      error:
-        "Failed to process your sentence. Please double check your sentence and try again",
-    };
-  }*/
 }
 
 export async function defineWord(prevState: any, formData: FormData) {
@@ -82,7 +50,6 @@ export async function defineWord(prevState: any, formData: FormData) {
   });
 
   if (!response.ok) {
-    //throw new Error("Failed to fetch data");
     var body = await response.json();
 
     return {
@@ -92,48 +59,9 @@ export async function defineWord(prevState: any, formData: FormData) {
   }
 
   var answer = await response.json();
-  //console.log("Answer:");
-  //console.log(answer);
+
   return {
     wordAnswer: answer,
     error: null,
   };
-  // This request should be refetched on every request.
-  // Similar to `getServerSideProps`.
-  /*try {
-    const response = await fetch(`${process.env.API_URL}/search/word`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(rawFormData),
-      cache: "no-store",
-    });
-
-    if (!response.ok) {
-      //console.error("Error:", body);
-      //throw new Error("Failed to fetch data");
-      var body = await response.json();
-
-      return {
-        wordAnswer: "",
-        error: body.message,
-      };
-    }
-
-    var answer = await response.json();
-    //console.log("Answer:");
-    //console.log(answer);
-    return {
-      wordAnswer: answer,
-      error: null,
-    };
-  } catch (error) {
-    console.error("Error:", error);
-    return {
-      wordAnswer: answer,
-      error:
-        "Failed to process your word. Please make sure you use the Dictionary to look up single words only",
-    };
-  }*/
 }
