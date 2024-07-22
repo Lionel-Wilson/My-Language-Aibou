d28d04e532cf212313b136c7bd28146e1be0ae10
Lionel Wilson <lionel_wilson@outlook.com>
2024-07-22 02:28:58 +0100

    Use state and markdown package to format sentence answer instead of setinnerhtml

--- src/app/search/components/sentence-analyser.tsx
+++ src/app/search/components/sentence-analyser.tsx
@@ -4,6 +4,9 @@ import { SubmitButton } from "@/app/components/submit-button";
 import { wellKnownLanguages } from "../utils/constants";
 import { useFormState } from "react-dom";
 import { analyseExpression } from "../actions";
+import { useState, useEffect } from "react";
+import { createRoot } from "react-dom/client";
+import Markdown from "react-markdown";
 import { formatExpression } from "../utils/functions";
 
 export function SentenceAnalyser() {
@@ -15,9 +18,18 @@ export function SentenceAnalyser() {
     }
   );
 
+  const [formattedExpression, setFormattedExpression] = useState<string>("");
+
+  useEffect(() => {
+    if (expressionAnaylsis?.expressionAnswer) {
+      setFormattedExpression(expressionAnaylsis.expressionAnswer);
+      console.log(expressionAnaylsis.expressionAnswer);
+    }
+  }, [expressionAnaylsis?.expressionAnswer]);
+
   return (
     <>
-      <div className=" bg-base-100 border-base-300 rounded-box p-6 max-w-xs sm:max-w-sm md:max-w-md ">
+      <div className="bg-base-100 border-base-300 rounded-box p-6 max-w-xs sm:max-w-sm md:max-w-md">
         <form action={expressionAnalysisAction}>
           <div>
             <label className="form-control">
@@ -29,14 +41,14 @@ export function SentenceAnalyser() {
               <div className="sm:flex sm:justify-center">
                 <textarea
                   name="sentence"
-                  className="textarea textarea-bordered textarea-xs min-[410px]:textarea-md w-full max-w-xs md:max-w-md sm:h-24 "
+                  className="textarea textarea-bordered textarea-xs min-[410px]:textarea-md w-full max-w-xs md:max-w-md sm:h-24"
                   placeholder="Paste your sentence here..."
                 ></textarea>
               </div>
             </label>
-            <div className="mt-3 sm:mt-5 sm:flex sm:justify-center ">
+            <div className="mt-3 sm:mt-5 sm:flex sm:justify-center">
               <button
-                className="btn btn-outline btn-xs min-[410px]:btn-sm "
+                className="btn btn-outline btn-xs min-[410px]:btn-sm"
                 type="reset"
               >
                 Clear Field
@@ -45,22 +57,6 @@ export function SentenceAnalyser() {
           </div>
 
           <div className="flex flex-wrap justify-items-start sm:flex sm:justify-center mt-2">
-            {/*<div> old tier selection
-                      <div className="label w-56 sm:w-72 ">
-                        <span className="label-text text-xs min-[410px]:text-sm md:text-base">
-                          Select tier (Determines detail)
-                        </span>
-                      </div>
-                      <select
-                        name="tier"
-                        className="select select-primary w-full select-xs min-[410px]:select-sm  max-w-xs  sm:max-w-md "
-                      >
-                        <option defaultValue="true" value="Premium">
-                          Premium
-                        </option>
-                        <option value="Basic">Basic</option>
-                      </select>
-                    </div>*/}
             <div>
               <div className="label w-56 sm:w-72">
                 <span className="label-text text-xs min-[410px]:text-sm md:text-base">
@@ -69,7 +65,7 @@ export function SentenceAnalyser() {
               </div>
               <select
                 name="nativeLanguage"
-                className="select select-primary w-full select-xs min-[410px]:select-sm  max-w-xs  sm:max-w-md"
+                className="select select-primary w-full select-xs min-[410px]:select-sm max-w-xs sm:max-w-md"
               >
                 {wellKnownLanguages.map((lang) => (
                   <option key={lang} value={lang}>
@@ -82,19 +78,18 @@ export function SentenceAnalyser() {
           <SubmitButton Title="Translate and Learn" />
 
           <div>
-            {expressionAnaylsis?.expressionAnswer ? (
-              <div className=" mt-5 sm:mt-10 text-xs min-[410px]:text-sm md:text-base bg-blue-100 p-2 rounded-md">
-                <p
-                  dangerouslySetInnerHTML={{
-                    __html: formatExpression(
-                      expressionAnaylsis.expressionAnswer
-                    ),
-                  }}
-                />
+            {formattedExpression ? (
+              <div
+                id="sentence-answer"
+                className="mt-5 sm:mt-10 text-xs min-[410px]:text-sm md:text-base bg-blue-100 p-2 rounded-md"
+              >
+                <p>
+                  <Markdown>{formattedExpression}</Markdown>
+                </p>
               </div>
             ) : null}
             {expressionAnaylsis?.error ? (
-              <div className=" mt-5 sm:mt-10 text-xs min-[410px]:text-sm text-red-600 flex justify-center">
+              <div className="mt-5 sm:mt-10 text-xs min-[410px]:text-sm text-red-600 flex justify-center">
                 {expressionAnaylsis.error}
               </div>
             ) : null}
